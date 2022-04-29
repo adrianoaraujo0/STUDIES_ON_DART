551b3f68428691db89f8cce28c9b447c167d7ef4
adryanfrancis1 <adriano.amedeiros@hotmail.com>
2022-04-28 22:07:17 -0300

    Listas

--- lista.dart
+++ lista.dart
@@ -1,34 +1,40 @@
 void main() {
- 
   List<int> inteiros = [];
 
   inteiros.add(4);
   inteiros.add(5);
   inteiros.add(77);
   inteiros.add(-70);
-  
+
   print('Elementos da lista:');
-  print(inteiros); 
+  print(inteiros);
   print('\n');
-  
+
   print('Tamanho da lista:');
   print(inteiros.length);
   print('\n');
-  
+
   print('Inserindo elemento na lista:');
   inteiros.insert(3, 4);
   print(inteiros);
   print('\n');
-  
+
   print('Verificando elemento da lista:');
   print(inteiros.contains(-70));
   print(inteiros.indexOf(-70));
   print('\n');
-  
+
   print('Removendo elemento da lista:');
   print(inteiros.remove(4));
   print(inteiros);
-  
-  
-  
+  print('\n');
+
+  print('Removendo elemento de uma posição especifica:');
+  print(inteiros.removeAt(3));
+  print(inteiros);
+  print('\n');
+
+  print('Embaralhando elemento da lista:');
+  inteiros.shuffle();
+  print(inteiros);
 }
